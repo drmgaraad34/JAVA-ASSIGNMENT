18d99884d0a114eed4d106378f840e3ebf2add20
drmgaraad34 <drmgaraad34@gmail.com>
2025-04-30 14:36:52 +0300

    Add files via upload

---
+++ DemoC1221287Application.java
@@ -0,0 +1,12 @@
+package com.ca226.demoC1221287;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoC1221287Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoC1221287Application.class, args);
+	}
+
+}
---
+++ Staff.java
@@ -0,0 +1,17 @@
+package com.ca226.demoC1221287;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Staff {
+    private Long id;
+    private String name;
+    private String role;
+
+}
